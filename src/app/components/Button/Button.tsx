43eac41e663aebd867fa22e9cf4303e12f669ffd
bragayuri <chefyuribraga@gmail.com>
2023-05-13 05:38:28 +0100

    feat[prettier]: installing prettier and lint config

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,24 +1,39 @@
 'use client'
+import React from 'react'
 import { SyntheticEvent } from 'react'
 import styles from './Button.module.scss'
 
 type ButtonProps = {
-    readonly id: string
-    readonly onClick: (event: SyntheticEvent) => void
-    readonly loading: boolean
-    readonly disabled: boolean
-    readonly children: React.ReactNode
-    readonly color?: 'blue' | 'red' | 'green'
-    readonly className?: string
-    
+  readonly id: string
+  readonly onClick: (event: SyntheticEvent) => void
+  readonly loading: boolean
+  readonly disabled: boolean
+  readonly children: React.ReactNode
+  readonly color?: 'blue' | 'red' | 'green'
+  readonly className?: string
 }
 
-const Button: React.FC<ButtonProps> = ({ id, onClick, loading, disabled, children, color = 'blue', className }) => {
-    return (
-        <button id={id} onClick={onClick} disabled={disabled} className={`${styles.button} ${loading && styles.loading} ${styles[color]} ${className}`}>
-        {children}
-        </button>
-    )
+const Button: React.FC<ButtonProps> = ({
+  id,
+  onClick,
+  loading,
+  disabled,
+  children,
+  color = 'blue',
+  className,
+}) => {
+  return (
+    <button
+      id={id}
+      onClick={onClick}
+      disabled={disabled}
+      className={`${styles.button} ${loading && styles.loading} ${
+        styles[color]
+      } ${className}`}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react'
 import styles from './page.module.scss'
 
-const CountryPediaHomePage = () => { 
+const CountryPediaHomePage = () => {
   return (
     <div className={styles.main}>
       <h1>Welcome to countrypedia</h1>
